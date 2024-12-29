ef4af6c1118028307dcf98e6b5b96c6904bbe762
Jessica Land <devonval06@gmail.com>
2024-12-28 20:37:06 -0700

    Adds 1 file for slope equation

---
+++ get_slope.js
@@ -0,0 +1,17 @@
+
+function get_slope (y, x, b) {
+    //y=mx+b
+    //y-b=mx 
+    //(y-b)/x=m
+    var slope;
+    slope = (y-b)/x;
+
+    return console.log(slope);
+}
+
+var y = 20; 
+var b = 10;
+var x = 5;
+
+get_slope(y, x, b) 
+
