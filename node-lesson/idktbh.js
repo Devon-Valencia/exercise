27c8ed8288a0ee7d5ee5d3416149fa49dd40c0d1
devon-valencia <devonval06@gmail.com>
2025-01-30 15:00:44 -0700

    Tells you if a number is even or odd

---
+++ node-lesson/idktbh.js
@@ -0,0 +1,10 @@
+function evenOrOdd(number) {
+    return number % 2 === 0 ? "even" : "odd";
+}
+
+console.log(evenOrOdd(4))
+console.log(evenOrOdd(1))
+console.log(evenOrOdd(43))
+console.log(evenOrOdd(3))
+console.log(evenOrOdd(6))
+console.log(evenOrOdd(24))
